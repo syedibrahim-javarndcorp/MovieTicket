1f8f025a7286a9fd5ff820e37990c02c00e48d49
syedibrahim-javarndcorp <syedibrahim.javarndcorp@gmail.com>
2021-12-21 17:33:43 -0500

    inital

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Movies(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    time = models.TimeField(auto_now=False, auto_now_add=False)
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,10 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'main.html')
+
+def userdata(request,pk):
+    return render(request, )
