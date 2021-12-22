267fbba7f6dae06fcc487e2532cf10d9d2c041e5
syedibrahim-javarndcorp <syedibrahim.javarndcorp@gmail.com>
2021-12-22 17:13:45 -0500

    init

---
+++ api/serializer.py
@@ -0,0 +1,8 @@
+# from django.db.models import fields
+# from rest_framework.serializers import ModelSerializer
+# from ..movie.models import Movies
+
+# class MovieSerializer(ModelSerializer):
+#     class Meta:
+#         model = Movies
+#         fields = '__all__'
