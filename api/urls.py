a3e179c2ba265fb537df8c7cec0f2f99018f225a
syedibrahim-javarndcorp <syedibrahim.javarndcorp@gmail.com>
2021-12-23 11:24:09 -0500

    init

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("",views.getroutes),
     path('movie/',views.getmovies),
     path('movie/<str:pk>',views.getmovie),
+    path('movie/<str:pk>/title',views.getsinglemovie),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,15 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 from .serializer import MovieSerializer
 from movie.models import Movies
 
 from api import serializer
 
+
 @api_view(['GET'])
 def getroutes(request):
 
     routes = [
-        {'GET' : '/api/movie'},
+        {'GET': '/api/movie'},
     ]
     return Response(routes)
 
@@ -20,8 +22,24 @@ def getmovies(request):
     serializer = MovieSerializer(movie, many=True)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 def getmovie(request, pk):
-    movie = Movies.objects.get(id = pk)
+    movie = Movies.objects.get(id=pk)
     serializer = MovieSerializer(movie, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def getsinglemovie(request, pk):
+    movie = Movies.objects.get(id=pk)
+    data = request.data
+
+    movie, created = Movies.objects.get_or_create(
+        title=movie,
+    )
+    movie.value = data['title']
+    movie.save()
+    serializer = MovieSerializer(movie, many=False)
+    
     return Response(serializer.data)
